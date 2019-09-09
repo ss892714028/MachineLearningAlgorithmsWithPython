385287054f20675919f8bed46b805766e106b84a
ss892714028 <34635663+ss892714028@users.noreply.github.com>
2019-09-09 19:14:12 -0400

    initial commit

--- Core/PCA.py
+++ Core/PCA.py
@@ -1,8 +1,58 @@
 import numpy as np
 import Data as d
 
-class PCA:
-    def __init__(self, data, k):
-        self.data = np.array(data)
-        self.k = k
 
+class PCA:
+    def __init__(self, data, critical_value):
+        self.data = np.array(data)
+        self.critical_value = critical_value
+
+    def normalization(self):
+        data = self.data
+        mean = np.mean(data, axis=0)
+        std = np.std(data, axis=0)
+        for i in range(data.shape[1]):
+            if std[i] != 0:
+                data[:, i] = (data[:, i] - mean[i]) / std[i]
+            else:
+                data[:, i] = 0
+        return data, mean, std
+
+    def find_eig(self):
+        data, mean, std = self.normalization()
+        cov_matrix = np.cov(data.T)
+        eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+        # sort the eigenvalues, return its sorted index
+        order = np.absolute(eigenvalues).argsort()[::-1]
+        eigenvalues = eigenvalues[order]
+        eigenvectors = eigenvectors[order]
+
+        return eigenvalues, eigenvectors
+
+    def find_k(self, eigenvalues, eigenvectors):
+        counter = 0
+        while True:
+            counter += 1
+            if sum(eigenvalues[:counter])/sum(eigenvalues) > self.critical_value:
+                break
+        print('selected {} features'.format(counter))
+        e_vector = eigenvectors.T[:counter]
+        return e_vector
+
+    def pca(self):
+        data = self.data
+        eigenvalues, eigenvectors = self.find_eig()
+        e_vector = self.find_k(eigenvalues, eigenvectors)
+        z = self.projection_data(data, e_vector)
+        return z
+
+    @staticmethod
+    def projection_data(x, u):
+        return np.dot(x, u.T)
+
+
+if __name__ == '__main__':
+    test_data, test_label = d.loadData(r'C:\Users\Stan\PycharmProjects\MachineLearningAlgorithms\Data\mnist_test.csv')
+    reduce = PCA(test_data, 0.9)
+    z = reduce.pca()
+    print(z.shape)
