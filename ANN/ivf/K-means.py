5cc3b93b26b4ab69f0a9c546d608a93cc70bade5
sida shen <sida.shen@zilliz.com>
2022-02-22 10:55:23 +0800

    Add ANN
    Signed-off-by: sida shen <sida.shen@zilliz.com>

--- ANN/ivf/K-means.py
+++ ANN/ivf/K-means.py
@@ -1,10 +1,9 @@
 import numpy as np
 import copy
-import Data as d
 
 
 class Kmeans:
-    def __init__(self, test, k, max_iteration=100):
+    def __init__(self, test, k, max_iteration=20):
         self.test = np.array(test)
         self.k = k
         self.max_iteration = max_iteration
@@ -12,14 +11,14 @@ class Kmeans:
     @staticmethod
     def distance(x1, x2, axis=1):
         # L2 distance
-        return np.linalg.norm(x1 - x2,axis = axis)
+        return np.linalg.norm(x1 - x2, axis=axis)
 
     def get_centroid(self):
         data = self.test
         # randomly pick k number of data as initial centroids
         indx = np.random.randint(data.shape[0], size=self.k)
 
-        centroid = data[indx,:]
+        centroid = data[indx, :]
         return centroid
 
     def k_means(self):
@@ -51,47 +50,29 @@ class Kmeans:
                 # for each cluster, recalculate centroid position based on
                 # the mean value of data in that cluster
 
-                centroid[i] = np.mean([data[j] for j in range(data.shape[0]) if cluster_assignment[j] == i], axis=0)
-
+                # centroid[i] = np.mean([data[j] for j in range(data.shape[0]) if cluster_assignment[j] == i], axis=0)
+                centroid[i] = np.mean(data[np.where(cluster_assignment == i)], axis=0)
                 # record the distance between new centroid and old centroid
             check = self.distance(centroid, previous_centroid, axis=None)
             iterator += 1
-            print('current difference: {}'.format(check))
 
         result = []
         for i in range(self.k):
-            result.append([data[j] for j in range(data.shape[0]) if cluster_assignment[j] == i])
+            result.append(data[np.where(cluster_assignment == i)])
         return result
 
 
 if __name__ == '__main__':
-    test_data, test_label = d.loadData(r'C:\Users\Stan\PycharmProjects\MachineLearningAlgorithms\Data\mnist_test.csv')
-    k = Kmeans(test_data,k = 10)
+    nlist = 100
+    test_data = np.random.random([10000, 128])
+    k = Kmeans(test_data, k=nlist)
+    import time
+    t = time.time()
     results = k.k_means()
-    dict = {}
-    for i in range(len(test_label)):
-        dict[tuple(test_data[i])] = test_label[i]
-    result = []
-    for i in results:
-        temp = []
-        for j in i:
-            temp.append(dict[tuple(j)])
-        result.append(temp)
-    print(np.array([sorted(i) for i in result]))
-    for i in result:
-        # print occurance of each class in each cluster
+    print(f"sida time spent: {time.time()-t}")
 
-        print([list(i).count(j) for j in range(10)])
-
-#############
-# output
-# [1, 1101, 126, 50, 27, 80, 37, 79, 67, 28]
-# [728, 0, 13, 0, 1, 5, 20, 1, 7, 6]
-# [13, 3, 19, 1, 17, 7, 651, 0, 6, 4]
-# [6, 4, 31, 125, 1, 153, 2, 4, 632, 16]
-# [177, 0, 24, 58, 29, 258, 224, 1, 73, 6]
-# [48, 3, 44, 717, 0, 284, 4, 0, 110, 8]
-# [3, 0, 18, 16, 436, 43, 10, 172, 19, 427]
-# [3, 24, 735, 34, 0, 2, 5, 17, 11, 2]
-# [0, 0, 5, 4, 471, 56, 5, 45, 40, 484]
-# [1, 0, 17, 5, 0, 4, 0, 709, 9, 28]
+    from sklearn.cluster import KMeans
+    import time
+    t = time.time()
+    kmeans = KMeans(n_clusters=nlist, max_iter=20).fit(test_data)
+    print(f"sklearn time spent: {time.time() - t}")
